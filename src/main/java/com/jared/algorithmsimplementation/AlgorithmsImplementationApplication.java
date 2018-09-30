b75f6e22b6bde449ce2f7c22c7801f935c0b18f6
寿旭 <jared.shou@ibaiqiu.com>
2018-09-30 14:36:41 +0800

    算法实现

---
+++ src/main/java/com/jared/algorithmsimplementation/AlgorithmsImplementationApplication.java
@@ -0,0 +1,12 @@
+package com.jared.algorithmsimplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsImplementationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmsImplementationApplication.class, args);
+    }
+}
