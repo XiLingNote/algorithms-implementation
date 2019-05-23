d74c98fa913c7e01fa00c05e810bc5503dda3485
shou_suface <15721509625@163.com>
2019-05-23 23:52:04 +0800

    【冒泡排序】 增加冒泡排序代码

--- src/main/java/com/jared/algorithmsimplementation/search/BinarySearch.java
+++ src/main/java/com/jared/algorithmsimplementation/search/BinarySearch.java
@@ -29,22 +29,46 @@ public class BinarySearch {
         }
         return  -1;
     }
-    public void doubleArraySearch(int [][]array,int target){
-        for (int i = 0; i < array.length; i++) {
-            int [] arr = array[i];
-            int search = arraySearch(arr, target);
-            if(search != -1){
-                System.out.println("arr["+i+"]["+search+"]");
+
+    public boolean Find(int target, int [][] array) {
+        for(int i = 0;i<array.length;i++){
+            if(findByArray(target,array[i])!=-1) {
+                return true;
             }
         }
+        return false;
     }
+    public int findByArray(int target,int [] array){
+        int start = 0;
+        int end = array.length-1;
 
+        while(start <= end){
+            int mid = (start+end)/2;
+            int val = array[mid];
+
+            if( val== target){
+                return mid;
+            }
+            if(val > target){
+                end = mid -1;
+            }else if(val < target){
+                start = mid+1;
+            }
+        }
+        return -1;
+    }
+    /**
+     * 二分查找的
+     * @param args
+     */
     public static void main(String[] args) {
         BinarySearch b = new BinarySearch();
-        int[] array={1,3,4,5,6,7,8,9,20};
-        int [][] doubleArray ={{1,2,3},{1,5,6},{7,8,9}};
+        int [][] doubleArray = {{1,2,3},{1,5,6},{7,8,9}};
+        int[] array = {1,2,3};
 
-        int i = b.arraySearch(array, 10);
-        System.out.println(i);
+        for (int i = 0; i < 49; i++) {
+            System.out.println(i+"--"+b.Find(i,doubleArray));
+        }
+
     }
 }
--- src/main/java/com/jared/algorithmsimplementation/sort/BubbleSort.java
+++ src/main/java/com/jared/algorithmsimplementation/sort/BubbleSort.java
@@ -5,10 +5,34 @@ package com.jared.algorithmsimplementation.sort;
   * @author jared
   * @date 2019-05-22 13:53
   * @param
-  * @return
+  *
+  *
+  * 原理：比较两个相邻的元素，将值大的元素交换至右端。
+  *
+  * 思路：依次比较相邻的两个数，将小数放在前面，大数放在后面。即在第一趟：首先比较第1个和第2个数，将小数放前，大数放后。
+  *  然后比较第2个数和第3个数，将小数放前，大数放后，如此继续，直至比较最后两个数，将小数放前，大数放后。
+  *  重复第一趟步骤，直至全部排序完成。
   **/
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] array={1,4,3,4,5,6,7,8,9,20};
+        int[] array={9,1,4,3,4,5,6,7,8,9,20};
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 1; j < array.length-i; j++) {
+                //如果数据倒叙
+                if(array[j]<array[j-1]){
+                    int temp = array[j-1];
+                    array[j-1] = array[j];
+                    array[j] =  array[temp];
+                }
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+
+
     }
+
+
 }
