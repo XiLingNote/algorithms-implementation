541475fcabb11a9fc0ab7f2fd2529bc3a91d5402
shouxu <shouxu@enmonster.com>
2022-03-04 17:44:33 +0800

    【20220303】希尔排序

--- src/main/java/com/jared/algorithmsimplementation/sort/ShellSort.java
+++ src/main/java/com/jared/algorithmsimplementation/sort/ShellSort.java
@@ -1,4 +1,7 @@
 package com.jared.algorithmsimplementation.sort;
+
+import java.util.Arrays;
+
 /**
  *一、概念及其介绍
  * 希尔排序(Shell Sort)是插入排序的一种，它是针对直接插入排序算法的改进。
@@ -10,10 +13,51 @@ package com.jared.algorithmsimplementation.sort;
  * 二、适用说明
  * 希尔排序时间复杂度是 O(n^(1.3-2))，空间复杂度为常数阶 O(1)。希尔排序没有时间复杂度为 O(n(logn)) 的快速排序算法快 ，因此对中等大小规模表现良好，但对规模非常大的数据排序不是最优选择，总之比一般 O(n^2 ) 复杂度的算法快得多。
  *
- * 三、过程图示
+ *
  *
  * @Author shouxu
  * @Date 2022/3/4 16:05
  */
 public class ShellSort {
+    public static void main(String[] args) {
+
+        int[] arr={9,1,4,3,4,5,6,7,8,9,20};
+        ShellSort.shellSort_20220304(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static int [] shellSort_20220304(int [] array){
+        for (int gap = array.length / 2; gap > 0; gap = gap / 2) {
+            for (int i = 0; i < array.length; i = i + gap) {
+                for (int j = i; j > 0 ;j -= gap) {
+                    System.out.println("j:"+j);
+                    if(array[j]<array[j-gap]){
+                        int temp = array[j];
+                        array[j] = array[j-gap];
+                        array[j-gap] = temp;
+                    }
+                }
+            }
+        }
+        return array;
+    }
+
+    public static void sort(Comparable[] arr) {
+        int j;
+        for (int gap = arr.length / 2; gap >  0; gap /= 2) {
+            for (int i = gap; i < arr.length; i++) {
+                Comparable tmp = arr[i];
+                for (j = i; j >= gap && tmp.compareTo(arr[j - gap]) < 0; j -= gap) {
+                    arr[j] = arr[j - gap];
+                }
+                arr[j] = tmp;
+            }
+        }
+    }
+
+
+    public static void shellSort(int [] array){
+
+    }
+
 }
